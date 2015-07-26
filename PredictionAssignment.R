9d742de831fe9e4791b67dd5d655a109235b4585
yenlaw <yenlaw@zoho.com>
2015-07-26 23:15:36 +0100

    forassignmentassessment

--- PredictionAssignment.R
+++ PredictionAssignment.R
@@ -1,27 +1,29 @@
+  #dependencies
+
 library(caret)
 library(RCurl)
 
   # get Data using Curl and persist CSVs.
-data <- getURL("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv",
-               ssl.verifypeer=0L, followlocation=1L)
+#data <- getURL("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv",
+#               ssl.verifypeer=0L, followlocation=1L)
 
-writeLines(data,'pml-training.csv')
+#writeLines(data,'pml-training.csv')
 #read.csv('pml-training.csv')
 
-data <- getURL("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv",
-               ssl.verifypeer=0L, followlocation=1L)
+#data <- getURL("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv",
+#               ssl.verifypeer=0L, followlocation=1L)
 
-writeLines(data,'pml-testing.csv')
+#writeLines(data,'pml-testing.csv')
 #read.csv('pml-testing')
 
   # clean csv; remove CSVs and DIV by zero.
 trainingdata <- read.csv('pml-training.csv', na.strings=c("NA","#DIV/0!", ""))
-testdata <- read.csv('pml-testing.csv' , na.strings=c("NA", "#DIV/0!", ""))
+testdata <- read.csv('pml-testing.csv', na.strings=c("NA", "#DIV/0!", ""))
 
-summary(trainingdata,1)
-summary(testdata,1)
+#summary(trainingdata,1)
+#summary(testdata,1)
 
-  #remove rows where most columns are NA
+  #remove rows where >70% columns are NA
 trainnna <- which((colSums(!is.na(trainingdata)) >= 0.7 * nrow(trainingdata)))
 testnna <- which((colSums(!is.na(testdata)) >= 0.7 * nrow(testdata)))
 
@@ -32,37 +34,39 @@ cleantestdata     <- testdata[,testnna]
 #cleantrainingdata_a <- cleantrainingdata[,-c(1,2,3,4,5,6,7)]
 #cleantestdata_a     <- cleantestdata[,-c(1,2,3,4,5,6,7)]
 
+  #final column canditates
 cleantrainingdata_a <- cleantrainingdata[,-c(1,5)]
 cleantestdata_a     <- cleantestdata[,-c(1,5)]
 
-summary(cleantrainingdata_a,10)
-summary(cleantestdata_a,10)
+#summary(cleantrainingdata_a,10)
+#summary(cleantestdata_a,10)
 
-# remove problem id
-testSrc <- testSrc[-ncol(testSrc)]
-# fix factor levels
-testSrc$new_window <- factor(testSrc$new_window, levels=c("no","yes"))
-
+  #Use a 60:40 trainng:test ratio.
 inTraining  <- createDataPartition(cleantrainingdata_a$classe, p = 0.6, list = F)
-training    <- cleantrainingdata_a[inTraining, ]
-testing     <- cleantrainingdata_a[-inTraining, ]
+training    <- cleantrainingdata_a[inTraining,]
+testing     <- cleantrainingdata_a[-inTraining,]
 
+# Use Random Forest method
 rf_model<-train(classe~.,data=training,method="rf",
                 trControl=trainControl(method="cv",number=4), allowParallel=T)
 
-smallValidData <- subset(validData, 
-                         select=c(roll_belt, pitch_forearm, yaw_belt, magnet_dumbbell_y, pitch_belt, magnet_dumbbell_z, roll_forearm, accel_dumbbell_y, 
-                                  roll_dumbbell, magnet_dumbbell_x,classe))
+# Its slow ...
 
-smallModel <- train(classe ~ ., data=smallValidData[inTrain,], model="rf", trControl=trainControl(method="cv",number=4), allowParallel=T)
-
+# Calculate variable importance for column objects produced by train and model specific methods.
+# ... Could use result set to improve model creation efficiency.
 plot(varImp(rf_model))
 
+# Test Model
 pred <- predict(rf_model, newdata=testing)
 
-cnfMatrix <- confusionMatrix(testing$classe, pred)
-confMatrix <- confusionMatrix(pred,testing$classe)$table
+confMatrix <- confusionMatrix(pred,testing$classe)
 
+# Output Results.
 confMatrix
-prop.table(confMatrix)
-cnfMatrix$overall[1]
+#prop.table(confMatrix)
+
+# Output Accuracy.
+confMatrix$overall[1]
+
+# Used a Random Forest model and therefore there is no need for cross-validation.
+# There is also no need for a separate Test Set to get an unbiased estimate of the Test Set Error.
